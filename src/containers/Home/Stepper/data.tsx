1ae5b21a4dd581d84ebe23cc7b179ef9ac75b921
Serine-t <serinetovmasyan98@gmail.com>
2024-01-10 11:44:56 +0400

    adding places

--- src/containers/Home/Stepper/data.tsx
+++ src/containers/Home/Stepper/data.tsx
@@ -10,17 +10,20 @@ export const steps = [{
   text: 'Church Ceremony!💍',
   time: '13:30',
   path: 'https://yandex.com/maps/org/141175950094/?ll=44.863637%2C40.141879&z=13.24',
+  place: 'Geghard Monastery',
 },
 {
   icon: () => <Icon src={zags} />,
   text: 'Wedding Ceremony!',
   time: '15:15',
   path: 'https://yandex.com/maps/org/200050986571/?ll=44.483207%2C40.188295&z=17.09',
+  place: 'Sports and Concerts Complex name after Karen Demirchyan',
 }, {
   icon: () => <Icon src={dance} />,
   text: 'Let\'s party begin!😎',
   time: '18:00',
   path: 'https://yandex.com/maps/org/76916737047/?ll=44.533809%2C40.192637&z=17.29',
+  place: 'Souvenir Restaurant and Cafe',
 
 }, {
   icon: () => <Icon src={cake} />,
--- src/containers/Home/Stepper/index.tsx
+++ src/containers/Home/Stepper/index.tsx
@@ -2,6 +2,7 @@ import { Stepper, Step, StepLabel } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import StyledTypography from '@containers/common/StyledTypography';
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
 
 import { steps } from './data';
 import { StyledLine, StyledLink } from './styled';
@@ -10,12 +11,16 @@ const MyStepper = () => {
   return (
     <Stack maxWidth="500px" p="40px 20px">
       <Stepper activeStep={2} orientation="vertical" connector={<StyledLine />}>
-        {steps.map(({ icon, text, time, path }) => (
+        {steps.map(({ icon, text, time, path, place }) => (
           <Step key={text} sx={{ width: '100%', display: 'flex', alignItems: 'flex-start' }}>
             <StepLabel StepIconComponent={icon}>
               <Stack width="100%">
-                <StyledTypography variant="h6">{time}</StyledTypography>
+                <StyledTypography variant="h8">{time}</StyledTypography>
                 <Typography variant="h9">{text}</Typography>
+                <Stack direction="row" gap="6px">
+                  <PlaceOutlinedIcon />
+                  <StyledTypography variant="subtitle3">{place}</StyledTypography>
+                </Stack>
                 { path && (<StyledLink variant="body3" href={path} target="_blank">See On Map</StyledLink>)}
               </Stack>
             </StepLabel>
