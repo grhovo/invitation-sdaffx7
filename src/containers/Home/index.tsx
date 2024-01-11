779ee7cd88e5e1180232a96c4b5d68638afae2d1
Serine-t <serinetovmasyan98@gmail.com>
2024-01-11 12:36:31 +0400

    loading

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,8 +1,10 @@
-import { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 
 import Stack from '@mui/material/Stack';
 import NotFound from '@containers/NotFound';
 import { useParams } from 'react-router-dom';
+import background from '@assets/images/background.png';
+import canva from '@assets/images/canva.png';
 
 import MyStepper from './Stepper';
 import Timer from './Timer';
@@ -15,6 +17,46 @@ import { StyledContainer } from './styled';
 const Home = () => {
   const { id } = useParams();
   const guest = guestList.find((item) => item.id === id);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    const imageUrls = [
+      background,
+      canva,
+    ];
+
+    async function loadImages() {
+      try {
+        await Promise.all(imageUrls.map(loadImage));
+        setImagesLoaded(true);
+        console.log('All images have loaded.');
+      } catch (error) {
+        console.error('Failed to load images:', error);
+      }
+    }
+
+    loadImages();
+  }, []);
+
+  function loadImage(src:string) {
+    return new Promise((resolve, reject) => {
+      const image = new Image();
+
+      image.onload = () => {
+        resolve(image);
+      };
+
+      image.onerror = () => {
+        reject(new Error(`Failed to load image: ${src}`));
+      };
+
+      image.src = src;
+    });
+  }
+
+  if (!imagesLoaded) {
+    return null;
+  }
 
   if (!guest) {
     return <NotFound />;
@@ -32,7 +74,6 @@ const Home = () => {
         </Stack>
       </Stack>
     </StyledContainer>
-
   );
 };
 
