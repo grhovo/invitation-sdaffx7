b53ce39fe64456b530dd2df129d045fe84043ed2
Serine-t <serinetovmasyan98@gmail.com>
2024-08-21 12:53:24 +0400

    Img section

--- src/containers/Home/OrnamentSection/index.tsx
+++ src/containers/Home/OrnamentSection/index.tsx
@@ -1,9 +1,10 @@
 import img from '@assets/images/ornament.jpg';
-import { Stack, Typography } from '@mui/material';
+import taImg from '@assets/images/TA.png';
+import { Stack } from '@mui/material';
 
 const OrnamentSection = () => {
   return (
-    <Stack direction="row" alignItems="center" justifyContent="center" spacing={5} py="40px">
+    <Stack direction="row" alignItems="center" justifyContent="center" spacing={1} py="40px">
       <img
         src={img}
         alt=""
@@ -13,32 +14,14 @@ const OrnamentSection = () => {
 
         }}
       />
-      <Stack sx={{ position: 'relative' }} direction="row">
-        <Typography
-          sx={{
-            position: 'absolute', fontSize: '42px', left: '-35px', top: '-40px', color: '#c8a663',
-          }}
-        >
-          Թ
-        </Typography>
-        <Typography
-          sx={{ position: 'absolute',
-            fontSize: '60px',
-            left: '-22px',
-            top: '-74px',
-            fontFamily: 'ArmAllegro',
-            opacity: '60%',
-            color: '#c8a663' }}
-        >
-          &
-        </Typography>
-        <Typography
-          sx={{ position: 'absolute', fontSize: '42px', left: '5px', top: '-30px', color: '#c8a663' }}
-        >
-          Ա
-        </Typography>
-      </Stack>
-
+      <img
+        src={taImg}
+        alt=""
+        style={{
+          width: '70px',
+          objectFit: 'contain',
+        }}
+      />
       <img
         src={img}
         alt=""
